07cf64d4159e35f473e54a6cea902fbfc817c77a
Shiv Tavker <tavkershiv@gmail.com>
2018-06-13 01:27:24 +0530

    First Commit

---
+++ js/placements.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".dropdown-menu li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+
+  $("#departments li").click(function() {
+  	var active = $(".course-active");
+  	active.hide();
+  	active.removeClass('course-active');
+  	var id = $(this).attr('value');
+  	var depart = $(this).html();
+  	$('#'+id).show();
+  	 $('#'+id).addClass('course-active');
+  	 $('#depart').html(depart);
+  }
+  	);
+});
